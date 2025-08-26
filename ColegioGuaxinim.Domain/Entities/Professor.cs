728836663ee37cef873daa424938a961442bf132
douglas.acioli@gmail.com <douglas.acioli@gmail.com>
2025-08-25 21:41:09 -0300

    DBContext,Mapeamento ef core + propriedade que sera usada para controlar o tempo em que a importação estará bloqueada.

--- ColegioGuaxinim.Domain/Entities/Professor.cs
+++ ColegioGuaxinim.Domain/Entities/Professor.cs
@@ -4,6 +4,7 @@
     {
         public int Id { get; set; }
         public string Nome { get; set; }
+        public DateTime? BloquearTempoDeImportacao { get; set; }
         public ICollection<Aluno> Alunos { get; set; } = new List<Aluno>();
     }
 }
---
+++ ColegioGuaxinim.Infrastructure/Data/GuaxinimDbContext.cs
@@ -0,0 +1,33 @@
+﻿using ColegioGuaxinim.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace ColegioGuaxinim.Infrastructure.Data
+{
+    public class GuaxinimDbContext : DbContext
+    {
+        public GuaxinimDbContext(DbContextOptions<GuaxinimDbContext> options) : base(options){}
+        public DbSet<Professor> Professores => Set<Professor>();
+        public DbSet<Aluno> Alunos => Set<Aluno>();
+
+        protected override void OnModelCreating(ModelBuilder mb)
+        {
+            mb.Entity<Professor>(e =>
+            {
+                e.ToTable("Professor");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Nome).IsRequired().HasMaxLength(120);
+                e.Property(x => x.BloquearTempoDeImportacao);
+                e.HasMany(x => x.Alunos).WithOne(a => a.Professor).HasForeignKey(a => a.ProfessorId);
+            });
+
+            mb.Entity<Aluno>(e =>
+            {
+                e.ToTable("Aluno");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.Nome).IsRequired().HasMaxLength(120);
+                e.Property(x => x.Mensalidade).HasPrecision(18, 2);
+                e.Property(x => x.DataDeVencimento).IsRequired();
+            });
+        }
+    }
+}
